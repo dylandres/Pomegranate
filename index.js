c57545ee7fdda85d70ae5d5e8f6799c23a5c6607
Ulfeen Ayevan <uayevan01@gmail.com>
2021-10-28 12:05:57 -0400

    changed uri

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
 const app = express();
-//require('./database');
 
+var corsOptions = {
+    origin: 'http://localhost:4000/'
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
-// API
-//const users = require('/api/users');
-//app.use('/api/users', users);
-
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'), "index.html")
+const db = require('./server/db/server.js');
+db.mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to the database!");
+}).catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+app.use('/api', require('./server/routes/api'));
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.use('*', express.static(path.join(__dirname, "client", "build")));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
