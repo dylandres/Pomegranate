27f13dd124cfa551f3823d4411e1e072993d5b99
Dylan Andres <dylanrandres@gmail.com>
2021-10-27 21:02:58 -0400

    tabs

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Homepage.css';
 import  { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function HomePage() {
 
     return (
         <body>
             <div class='feed'>
-                feed
+                <Tabs>
+                    <TabList>
+                        <Tab>Popular Quizzes</Tab>
+                        <Tab>For You</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <h2>item 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>item 1</h2>
+                    </TabPanel>
+                </Tabs>
             </div>
             <div class='subscriptions'>
                 subscriptions
