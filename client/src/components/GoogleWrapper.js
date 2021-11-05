89fef063f4211dc8f90f954fbc14abadf7c1fd75
Joseph Tan <josephnestor.tan@stonybrook.edu>
2021-11-05 03:27:01 -0400

    added names and err handle

--- client/src/components/GoogleWrapper.js
+++ client/src/components/GoogleWrapper.js
@@ -5,11 +5,10 @@ import axios from 'axios'
 function GoogleWrapper() {
 
     const handleLoginSuccess = (res) => {
-        // const {userObj} = {email: res.profileObj.email, subscriptions: [], profile: -1, awards: []}
-        // console.log({userObj})
-        const userObj = {email: res.profileObj.email}
+
+        const userObj = {email: res.profileObj.email, subscriptions: [], awards: [], givenName: res.profileObj.givenName, famName: res.profileObj.familyName}
         console.log(userObj)
-        axios.post('http://localhost:4000/login', userObj)
+        axios.post('api/login', userObj)
     }
     return (
         <div>
@@ -17,7 +16,7 @@ function GoogleWrapper() {
             clientId = '954435352392-10dqjijkaksqlb0fb482e7hgapcg1nji.apps.googleusercontent.com'
             buttonText= 'Login'
             onSuccess = {handleLoginSuccess}
-            onFailure = {() => console.log ('ok')}
+            onFailure = {(err) => console.log ('err')}
             cookiePolicy = {'single_host_origin'}
             />
         </div>
