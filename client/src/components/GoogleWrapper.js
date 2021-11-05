e78883d91baf251e42e92f9ae361872fb2eef47e
Joseph Tan <josephnestor.tan@stonybrook.edu>
2021-11-04 23:44:31 -0400

    google login button added, functionality not yet working

--- client/src/components/GoogleWrapper.js
+++ client/src/components/GoogleWrapper.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login'
+import axios from 'axios'
 
 function GoogleWrapper() {
+
+    const handleLoginSuccess = (res) => {
+        // const {userObj} = {email: res.profileObj.email, subscriptions: [], profile: -1, awards: []}
+        // console.log({userObj})
+        const userObj = {email: res.profileObj.email}
+        console.log(userObj)
+        axios.post('http://localhost:4000/login', userObj)
+    }
     return (
         <div>
             <GoogleLogin
             clientId = '954435352392-10dqjijkaksqlb0fb482e7hgapcg1nji.apps.googleusercontent.com'
             buttonText= 'Login'
-            onSuccess = {() => console.log('wow')}
+            onSuccess = {handleLoginSuccess}
             onFailure = {() => console.log ('ok')}
             cookiePolicy = {'single_host_origin'}
             />
