21a99b000cfdc373d7c37e1af69e91e1ee698383
Oliver Liu <oliver.liu@stonybrook.edu>
2021-11-06 15:31:25 -0400

    first commit

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component {
-render() {
-	return (
-	<div >
-		<Person name="kapil" eyeColor="blue" age="23"></Person>
-		<Person name="Sachin" eyeColor="blue" ></Person>
-		<Person name="Nikhil" age="23"></Person>
-		<Person eyeColor="green" age="23"></Person>
-	</div>
-	);
-}
-}
+// class App extends Component {
+// render() {
+// 	return (
+// 	<div >
+// 		<Person name="kapil" eyeColor="blue" age="23"></Person>
+// 		<Person name="Sachin" eyeColor="blue" ></Person>
+// 		<Person name="Nikhil" age="23"></Person>
+// 		<Person eyeColor="green" age="23"></Person>
+// 	</div>
+// 	);
+// }
+// }
 
-class Person extends Component {
-render() {
-	return (
-	<div>
-		<p> Name: {this.props.name} </p>
-		<p>EyeColor: {this.props.eyeColor}</p>
-		<p>Age : {this.props.age} </p>
-	</div>
-	)
-}
-}
+// class Person extends Component {
+// render() {
+// 	return (
+// 	<div>
+// 		<p> Name: {this.props.name} </p>
+// 		<p>EyeColor: {this.props.eyeColor}</p>
+// 		<p>Age : {this.props.age} </p>
+// 	</div>
+// 	)
+// }
+// }
 
-Person.defaultProps = {
-name: "Rahul",
-eyeColor: "deepblue",
-age: "45"
-}
+// Person.defaultProps = {
+// name: "Rahul",
+// eyeColor: "deepblue",
+// age: "45"
+// }
 
-export default App;
+// export default App;
