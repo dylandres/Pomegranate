343460daa0a8ae2027194457c6b58b174e898ab2
jotean <jtan9424@gmail.com>
2021-11-08 14:15:58 -0500

    now passing in image url

--- client/src/components/GoogleWrapper.js
+++ client/src/components/GoogleWrapper.js
@@ -4,18 +4,28 @@ import axios from 'axios'
 
 function GoogleWrapper() {
 
-    const handleLoginSuccess = (res) => {
-
-        const userObj = {email: res.profileObj.email, subscriptions: [], awards: [], givenName: res.profileObj.givenName, famName: res.profileObj.familyName}
-        console.log(userObj)
-        axios.post('api/login', userObj)
+    const googleAuth = ({ profileObj }) => {
+        console.log(profileObj)
+        axios({
+          method: "post",
+          url: "/api/login",
+          data: {
+            //googleId: profileObj.googleId,
+            email: profileObj.email,
+            firstName: profileObj.givenName,
+            lastName: profileObj.familyName,
+            profilePicture: profileObj.imageUrl
+          },
+        })
+          .then((res) => console.log(res.data))
+          .catch((err) => console.log(err));
     }
     return (
         <div>
             <GoogleLogin
             clientId = '954435352392-24bg4crh8bc1bkt4hbpq6ke6iadacv53.apps.googleusercontent.com'
             buttonText= 'Login'
-            onSuccess = {handleLoginSuccess}
+            onSuccess = {googleAuth}
             onFailure = {(err) => console.log (err)}
             cookiePolicy = {'single_host_origin'}
             />
