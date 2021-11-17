2d8b74cde73a9c13fb3a8dd689bacfbf03941a8a
Dylan Andres <dylanrandres@gmail.com>
2021-11-17 02:37:45 -0500

    leaderboard works, no styling yet

--- client/src/components/Quizpage.js
+++ client/src/components/Quizpage.js
@@ -5,20 +5,22 @@ import '../style/tabs.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { myContext } from '../Context.js'
+import { sortLeaderboard } from '../functions.js';
 
 function QuizPage() {
 
     const [quiz, setQuiz] = useState({});
     const {userObject, setUserObject} = useContext(myContext)
+    const [leaderboard, setLeaderboard] = useState({})
     // Edit mode privilege
     // const [canEdit, setCanEdit] = useState(false);
 
     const getQuiz = async(quizName) => {
         const thisQuiz = await axios.get(`/api/users/${quizName}/quiz`).then(res => res.data);
-        console.log('h1');
-        console.log(JSON.stringify(thisQuiz[0]));
-        console.log('h2');
         setQuiz(thisQuiz[0]);
+        // Get leaderboard information for quiz
+        const board = sortLeaderboard(thisQuiz[0]);
+        setLeaderboard(board);
     }
 
     const parse = (url) => {
@@ -54,6 +56,16 @@ function QuizPage() {
                 Times Taken: {quiz.timesTaken}
                 <br/>
                 Rating: {(quiz.totalRating / quiz.totalVotes).toFixed(1)}
+                <div className="leaderboard">
+                Leaderboard
+                <br></br>
+                {
+                Object.entries(leaderboard).map( ([player, score], i) => 
+                    <p>
+                        {i+1}: {player}: {score}
+                    </p>
+                )}
+                </div>
                 <div class="rating">
                 <div class="rating-upper" style={{width: `${calculateRating(quiz)}%`}}>
                     <span>★</span>
