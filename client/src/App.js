a26eb51c45cf0fed2b61673c6f70e5e55ed4cfca
Joseph Tan <josephnestor.tan@stonybrook.edu>
2021-11-19 12:07:57 -0500

    redirect to homepage for invalid url

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css';
 import NavBar from './components/Navbar.js'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import DashBoard from './components/Dashboard.js'
 import SearchResults from './components/Searchresults.js'
 import ProfilePage from './components/Profilepage.js'
@@ -27,6 +27,8 @@ function App() {
         <Route path='/quiztaking' component={QuizTaking}/>
         {/* Platform Page */}
         <Route path='/platform' component={PlatformPage}/>
+        {/* Invalid URL redirects to homepage */}
+        <Route render={() => <Redirect to={{pathname: "/"}} />} />
       </body>
     </Router>
   );
