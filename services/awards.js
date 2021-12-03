dd40039a4487a05fb19e291402ec73959bff56a4
Dylan Andres <dylanrandres@gmail.com>
2021-12-03 02:17:35 -0500

    created subscriber awards, and quiz creation awards

--- services/awards.js
+++ services/awards.js
@@ -1,5 +1,6 @@
 const Award = require('../server/db/models/award.model.js')
 const User = require('../server/db/models/user.model.js')
+const Platform = require('../server/db/models/platform.model.js')
 
 //awardType - string in form "awardType:val" where val is 0 for award types that do not require a val
 const handleAward = async (user, award) => {
@@ -9,11 +10,42 @@ const handleAward = async (user, award) => {
     if(type === "quizzesTaken") {
         //get number of quizzes taken by turning quiz history into set and counting
         const quizzes = []
-        for(i = 0; i < user.quizHistory.length; i++) {
+        for(let i = 0; i < user.quizHistory.length; i++) {
             if(!(quizzes.includes(user.quizHistory[i].quizid)))
                 quizzes.push(user.quizHistory[i].quizid)
         }
         if(quizzes.length >= val) {
+            //put this user id into the award user[]
+            await Award.updateOne({'_id' : award._id}, { $push: {users: user._id}})
+            //and put this award into the user
+            await User.updateOne({'_id' : user._id}, { $push: {awards: award._id}})
+        }
+    }
+    else if (type === "subscribers") {
+        // get all platforms owned by this user
+        const platforms = await Platform.find({ 'ownerID': user._id })
+        // add up this user's subscriber count across all platforms
+        var subCount = 0;
+        for (let i = 0; i < platforms.length; i++) {
+            subCount += platforms[i].subscribers.length;
+        }
+        if (subCount >= val) {
+            //put this user id into the award user[]
+            await Award.updateOne({'_id' : award._id}, { $push: {users: user._id}})
+            //and put this award into the user
+            await User.updateOne({'_id' : user._id}, { $push: {awards: award._id}})
+        }
+    }
+    else if (type == "quizzes") {
+        // get all platforms owned by this user
+        const platforms = await Platform.find({ 'ownerID': user._id })
+        // for each platform, count up the number of quizzes created
+        var quizCount = 0;
+        for (let i = 0; i < platforms.length; i++) {
+            quizCount += platforms[i].quizzes.length;
+        }
+        console.log("this dude got " + quizCount + " quizzes");
+        if (quizCount >= val) {
             //put this user id into the award user[]
             await Award.updateOne({'_id' : award._id}, { $push: {users: user._id}})
             //and put this award into the user
