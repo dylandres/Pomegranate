ee02c18197047f1a4a9f95f3476c6dd13ab97611
jotean <jtan9424@gmail.com>
2021-12-12 21:37:17 -0500

    display a 404 page when there is an invalid URL

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css';
 import NavBar from './components/Navbar.js'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import DashBoard from './components/Dashboard.js'
 import SearchResults from './components/Searchresults.js'
 import ProfilePage from './components/Profilepage.js'
@@ -8,6 +8,7 @@ import PlatformPage from './components/Platformpage.js'
 import QuizPage from './components/Quizpage.js'
 import QuizTaking from './components/Quiztaking.js'
 import EditQuiz from './components/EditQuiz.js'
+import NotFound from './components/NotFound.js'
 
 function App() {
   return (
@@ -16,20 +17,23 @@ function App() {
       {/* NavBar always at top of page */}
         <NavBar/>
         <br/>
-        {/* DashBoard */}
-        <Route path="/" exact component={DashBoard}/>
-        {/* Search Results Page */}
-        <Route path='/search' component={SearchResults}/>
-        {/* Profile Page */}
-        <Route path='/profile' component={ProfilePage}/>
-        {/* Quiz Page */}
-        <Route path='/quizpage' component={QuizPage}/>
-        {/* Quiz Editing */}
-        <Route path='/quizediting' component={EditQuiz}/>
-        {/* Quiz Taking */}
-        <Route path='/quiztaking' component={QuizTaking}/>
-        {/* Platform Page */}
-        <Route path='/platform' component={PlatformPage}/>
+        <Switch>
+          {/* DashBoard */}
+          <Route exact path="/" component={DashBoard}/>
+          {/* Search Results Page */}
+          <Route path='/search' component={SearchResults}/>
+          {/* Profile Page */}
+          <Route path='/profile' component={ProfilePage}/>
+          {/* Quiz Page */}
+          <Route path='/quizpage' component={QuizPage}/>
+          {/* Quiz Editing */}
+          <Route path='/quizediting' component={EditQuiz}/>
+          {/* Quiz Taking */}
+          <Route path='/quiztaking' component={QuizTaking}/>
+          {/* Platform Page */}
+          <Route path='/platform' component={PlatformPage}/>
+          <Route path="*" component={NotFound} />
+        </Switch>
       </body>
     </Router>
   );
